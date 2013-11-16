4b934a5d3f4ba1df5b7176d779376cba3b5a3219
Julien Lafont <jla@zenexity.com>
2013-11-16 01:00:21 +0100

    fix

--- app/assets/javascripts/mediatheque/controllers/latests.js
+++ app/assets/javascripts/mediatheque/controllers/latests.js
@@ -7,5 +7,23 @@ angular.module('app.mediatheque')
 
         $scope.lastEntries = lastEntries
 
+        $('#movieModal').modal()
+
+        $scope.detail = function(entry) {
+
+          $scope.modalLoading = true
+          $('#movieModal').modal('show')
+
+          mediathequeService.movieDetail(entry.name)
+            .success(function(data) {
+              $scope.movie = data
+              $scope.selectedEntry = entry
+              $scope.modalLoading = false
+            })
+            .error(function(data) {
+              $scope.movie = null
+              $scope.modalLoading = false
+            })
+        }
       }]
   )
